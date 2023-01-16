cc1f6002f88edd30301472f299aa6cc8886b2a8d
wliu40 <wliu0828@gmail.com>
2023-01-16 07:43:11 -0500

    pipelien success

--- playground/data_prep.py
+++ playground/data_prep.py
@@ -13,11 +13,17 @@ saved_dir = args.preped_data
 run = Run.get_context()
 
 df = run.input_datasets['raw_data'].to_pandas_dataframe()
+run.log(name='data shape', value=str(df.shape[0]))
 
 num_features = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure',
                 'TricepsThickness','SerumInsulin','BMI','DiabetesPedigree']
+
+num_cols = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree']
+
+run.log_list("numerical cols", num_cols)
+
 scaler = MinMaxScaler()
-df[num_features] = scaler.fit(df[num_features])
+df[num_features] = scaler.fit_transform(df[num_features])
 
 print('saving data to preped_data')
 os.makedirs(saved_dir, exist_ok=True)
--- playground/train.py
+++ playground/train.py
@@ -11,10 +11,10 @@ from azureml.core import Run, Model
 import joblib
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--training-data', type=str, dest='input_data', help='Input dataset for model')
+parser.add_argument('--training-data', type=str, dest='training_data', help='Input dataset for model')
 args = parser.parse_args()
 
-training_data_dir = args.input_data
+training_data_dir = args.training_data
 
 run = Run.get_context()
 
@@ -22,13 +22,14 @@ run = Run.get_context()
 # df = run.input_datasets['input_data'].to_pandas_dataframe()
 df = pd.read_csv(os.path.join(training_data_dir, 'preped_data.csv'))
 
-X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin',
-            'BMI','DiabetesPedigree','Age']].values
+features = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness',
+            'SerumInsulin','BMI', 'DiabetesPedigree', 'Age']
 
-y = df['Diabetic'].values
+# Separate features and labels
 
+X, y = df[features].values, df['Diabetic'].values
 
-X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=0)
 
 model = DecisionTreeClassifier().fit(X_train, y_train)
 
@@ -44,24 +45,27 @@ auc = roc_auc_score(y_test, y_scores[:, 1])
 run.log(name='auc', value=auc)
 
 # ROC
-fpr, tpr, threshold = roc_curve(y_test, y_score[:,1])
-fig, ax = plt.subplots(figsize=(6, 6))
-ax.plot([0,1], [0,1], 'k--')
-ax.plot(fpr, tpr)
-ax.title('ROC curve')
-ax.set_xlabel('FPR')
-ax.set_ylabel('TPR')
-run.log_image(name='roc_curve', plot=fig)
+fpr, tpr, threshold = roc_curve(y_test, y_scores[:,1])
+fig = plt.figure(figsize=(6, 4))
+# Plot the diagonal 50% line
+plt.plot([0, 1], [0, 1], 'k--')
+# Plot the FPR and TPR achieved by our model
+plt.plot(fpr, tpr)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+run.log_image(name = "ROC", plot = fig)
+plt.show()
 
 print('Saving model')
 os.makedirs('models', exist_ok=True)
 
 model_file_path = os.path.join('models', 'diabetes_model.pkl')
-joblib.dump(model, model_file_path)
+joblib.dump(value=model, filename=model_file_path)
 
-Model.register(workspace=ws,
-              model_path=model_file,
-              tags={"training context": "pipeline"},
-              properties={'AUC': auc, "ACC": acc},
-              model_Path=model_file_path)
+Model.register(workspace=run.experiment.workspace,
+               model_path=model_file_path,
+               model_name='diabetes_model',
+               tags={"training context": "pipeline_2"},
+               properties={'AUC': np.float(auc), "ACC": np.float(acc)})
 run.complete()
